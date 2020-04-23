71d101abb421a7fdb25928f4950ebe8b2ffbb011
Daniel Meza <daniel.meza29@yahoo.com>
2020-04-22 22:46:53 -0500

    Iterated data that was fetch from my food API and displayed in DOM

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,7 +1,24 @@
 fetch("http://localhost:8088/food")
   .then(foods => foods.json())
   .then(parsedFoods => {
+    debugger;
+
     for (let food of parsedFoods) {
-      console.log(food.name);
+      let foodContainer = document.querySelector(".foodList");
+      let foodComponent = "";
+      foodComponent = foodFactory(food);
+      foodContainer.innerHTML += foodComponent;
     }
   })
+
+
+
+const foodFactory = (food) => {
+  return `
+    <h1>${food.name}</h1>
+    <div>${food.ethnicity}</div>
+    <div>${food.category}</div>
+    `
+
+}
+
